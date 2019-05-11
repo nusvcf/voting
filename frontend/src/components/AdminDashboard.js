689793388a3b1b9dbbd3bc5ee04fc59936c96652
Yen Ter <leeyenter@gmail.com>
2019-05-11 11:07:39 +0800

    Add admin dashboard

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -60,28 +60,55 @@ class VotersPage extends Component {
     }
 }
 
-class BallotsPage extends Component {
+class BallotRow extends Component {
+    render() {
+        let ballot = this.props.ballot;
+        let btns = [];
 
+        switch (ballot.status) {
+            case "Completed":
+                btns = [
+                    <button>Results</button>,
+                    <button className='btn-secondary'>Invalidate</button>]
+                break;
+            case "Ongoing":
+                btns = [
+                    <button>Close Ballot</button>,
+                ];
+                break;
+        }
+
+        let names = ballot.names.map((item) => <li>{item[0]} <span className='percent-voted'>({item[1].toFixed(2)}%)</span></li>);
+
+        return <tr>
+            <td>{ballot.id}</td>
+            <td style={{ fontWeight: 'bold' }}>{ballot.position}</td>
+            <td><ul>{names}</ul></td>
+            <td>{ballot.max_votes}</td>
+            <td>
+                <span className={'status-' + ballot.status.replace(' ', '-').toLowerCase()}>{ballot.status}</span><br />
+                <span className='percent-voted'>{ballot.percent_vote.toFixed(2)}% voted</span>
+            </td>
+            <td className='tbl-btns'>
+                {btns}
+            </td>
+        </tr>
+    }
+}
+
+class BallotsPage extends Component {
     constructor() {
         super();
         this.state = {
-            voters: [
-                { 'id': '001', 'position': 'President', names: [], status: 'Completed' },  
-                { 'id': '002', 'position': 'Secretary', names: [], status: 'Upcoming' }]
+            ballots: [
+                { id: '001', position: 'President', names: [["Joshua Xie", 89.737]], max_votes: 1, status: 'Completed', percent_vote: 76.47 },
+                { id: '002', position: 'Outreach Coordinator', names: [["Winfred Tan", 64.489], ["Bertram Tan", 64.859]], max_votes: 2, status: 'Completed', percent_vote: 92.7328 },
+                { id: '003', position: 'Secretary', names: [["Ang Seng Peng", 84.847]], max_votes: 1, status: 'Ongoing', percent_vote: 6.473 }]
         }
     }
 
     render() {
-        let rows = this.state.voters.map((voter) => <tr>
-            <td>{voter.id}</td>
-            <td>{voter.position}</td>
-            <td>{voter.names}</td>
-            <td>{voter.status}</td>
-            <td className='tbl-btns'>
-                <button className='btn-secondary'>Invalidate</button>
-                <button className='btn-warn'>Delete</button>
-            </td>
-        </tr>)
+        let rows = this.state.ballots.map((ballot) => <BallotRow ballot={ballot} />)
 
         return <div id='ballots-page'>
             <button>+&nbsp;&nbsp;Create Ballot</button>
@@ -91,6 +118,7 @@ class BallotsPage extends Component {
                         <th>ID</th>
                         <th>Position</th>
                         <th>Names</th>
+                        <th>Max Votes</th>
                         <th>Status</th>
                         <th>&nbsp;</th>
                     </tr>
@@ -119,7 +147,7 @@ class AdminDashboard extends Component {
         return <div id='admin'>
             <Navbar page={this.state.page} changePage={this.changePage} />
             {this.state.page === 'voters' && <VotersPage />}
-            {this.state.page == 'ballots' && <BallotsPage />}
+            {this.state.page === 'ballots' && <BallotsPage />}
         </div>
     }
 }
