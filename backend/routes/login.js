7efe2a9e1abb87a3c64f6a4ccf499287c5526191
Lim Jie <limjie1994@gmail.com>
2019-05-13 02:27:14 +0800

    Added votes saving into excel

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,63 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const checkId = require('./checkId');
-const bcrypt = require('bcrypt');
+const checkId = require("./checkId");
+const bcrypt = require("bcrypt");
 
 //Log in authentication
-router.post('/', (req, res) => {
-	const ADMIN = 'admin';
-	const USER = 'user';
+router.post("/", (req, res) => {
+  const ADMIN = "admin";
+  const USER = "user";
 
-	//Error checking
-	const username = req.body.username;
-	const password = req.body.password;
-	const usernameToVoterIndex = req.app.locals.usernameToVoterIndex;
-	const voters = req.app.locals.voters;
+  //Error checking
+  const username = req.body.username;
+  const password = req.body.password;
+  const usernameToVoterIndex = req.app.locals.usernameToVoterIndex;
+  const voters = req.app.locals.voters;
 
-	let response = {
-		success: false
-	};
+  let response = {
+    success: false
+  };
 
-	let id = 'admin';
+  let id = "admin";
 
-	//Check if username is valid
-	if (username !== ADMIN) {
-		const idx = checkId(usernameToVoterIndex, username);
-		if (idx === -1) {
-			res.json(response);
-			return;
-		}
+  //Check if username is valid
+  if (username !== ADMIN) {
+    const idx = checkId(usernameToVoterIndex, username);
+    if (idx === -1) {
+      res.json(response);
+      return;
+    }
 
-		//Check if username and password tally
-		if (password !== voters[idx].password) {
-			res.json(response);
-			return;
-		}
-		id = voters[idx].id;
-	} else {
-		// Check if password is correct
-		let match = bcrypt.compareSync(password, req.app.locals.adminPwHash);
-		if (!match) {
-			res.json(response);
-			return;
-		} else {
-			req.app.locals.adminPw = password
-		}
-	}
+    //Check if username and password tally
+    if (password !== voters[idx].password) {
+      res.json(response);
+      return;
+    }
+    id = voters[idx].id;
+  } else {
+    // Check if password is correct
+    let match = bcrypt.compareSync(password, req.app.locals.adminPwHash);
+    if (!match) {
+      res.json(response);
+      return;
+    } else {
+      req.app.locals.adminPw = password;
+    }
+  }
 
-	//Successful, set session states
-	req.session.username = username;
-	req.session.userid = id;
-	if (username === ADMIN) {
-		req.session.userType = ADMIN;
-		response.userType = ADMIN;
-	} else {
-		req.session.userType = USER;
-		response.userType = USER;
-	}
-	response.success = true;
-	response.id = id;
-	res.json(response);
+  //Successful, set session states
+  req.session.username = username;
+  req.session.userid = id;
+  if (username === ADMIN) {
+    req.session.userType = ADMIN;
+    response.userType = ADMIN;
+    XlsxPopulate.fromBlankAsync().then(workbook => {
+      return workbook.toFileAsync("./Votes.xlsx", {
+        password: req.app.locals.adminPw
+      });
+    });
+  } else {
+    req.session.userType = USER;
+    response.userType = USER;
+  }
+  response.success = true;
+  response.id = id;
+  res.json(response);
 });
 
 module.exports = router;
