58a937e56556b7ebeab31f6ae8fbdc1deaf8d616
Yen Ter <leeyenter@gmail.com>
2019-05-13 18:20:57 +0800

    Encrypt data sources

--- backend/localPersistance.js
+++ backend/localPersistance.js
@@ -1,26 +1,57 @@
-const fs = require('fs');
-const crypto = require('crypto');
+const fs = require("fs");
+const crypto = require("crypto");
+
+function getCipherKey(password) {
+    return crypto
+        .createHash("sha256")
+        .update(password)
+        .digest();
+}
+
+const iv = crypto.randomBytes(16);
+
+function encrypt(text, pw) {
+    let key = crypto.createHash('sha256').update(String(pw)).digest('base64').substr(0, 32);
+    let cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString("hex"), encryptedData: encrypted.toString("hex") };
+}
+
+function decrypt(text, pw) {
+    let iv = Buffer.from(text.iv, "hex");
+    let key = crypto.createHash('sha256').update(String(pw)).digest('base64').substr(0, 32);
+    let encryptedText = Buffer.from(text.encryptedData, "hex");
+    let decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
 
 function save(data) {
-    let obj = {}
+    let obj = {};
     for (let key in data) {
-        if (key !== 'settings') {
+        if (key !== "settings") {
             obj[key] = data[key];
         }
     }
     // Save it
-    fs.writeFileSync('local.storage', JSON.stringify(obj))
+    fs.writeFileSync(
+        "local.storage",
+        JSON.stringify(encrypt(JSON.stringify(obj), data.adminPw))
+    );
 }
 
-function load() {
-    if (fs.existsSync('local.storage')) {
-        return JSON.parse(fs.readFileSync('local.storage'))
+function load(pw) {
+    if (fs.existsSync("local.storage")) {
+        let data = JSON.parse(decrypt(JSON.parse(fs.readFileSync("local.storage")), pw));
+        return data;
     } else {
-        return {}
+        return {};
     }
 }
 
 module.exports = {
-    save: save, 
+    save: save,
     load: load
-}
+};
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,86 +2,85 @@ const express = require("express");
 const router = express.Router();
 const checkId = require("./checkId");
 const bcrypt = require("bcrypt");
-const XlsxPopulate = require('xlsx-populate');
+const XlsxPopulate = require("xlsx-populate");
 
-var local = require('../localPersistance');
+var local = require("../localPersistance");
 
 //Log in authentication
 router.post("/", (req, res) => {
-  const ADMIN = "admin";
-  const USER = "user";
+    const ADMIN = "admin";
+    const USER = "user";
 
-  //Error checking
-  const username = req.body.username;
-  const password = req.body.password;
-  const usernameToVoterIndex = req.app.locals.usernameToVoterIndex;
-  const voters = req.app.locals.voters;
+    //Error checking
+    const username = req.body.username;
+    const password = req.body.password;
+    const usernameToVoterIndex = req.app.locals.usernameToVoterIndex;
+    const voters = req.app.locals.voters;
 
-  let response = {
-    success: false
-  };
+    let response = {
+        success: false
+    };
 
-  let id = "admin";
+    let id = "admin";
 
-  if (username !== ADMIN) {
-    // User 
-    const idx = checkId(usernameToVoterIndex, username);
-    if (idx === -1) {
-      res.json(response);
-      return;
-    }
+    if (username !== ADMIN) {
+        // User
+        const idx = checkId(usernameToVoterIndex, username);
+        if (idx === -1) {
+            res.json(response);
+            return;
+        }
 
-    //Check if username and password tally
-    if (password !== voters[idx].password) {
-      res.json(response);
-      return;
-    }
+        //Check if username and password tally
+        if (password !== voters[idx].password) {
+            res.json(response);
+            return;
+        }
 
-    // Check if voter is invalidated
-    if (!voters[idx].isValid) {
-      res.json(response);
-      return;
-    }
-    id = voters[idx].id;
-  } else {
-    // Admin
-    // Check if password is correct
-    let match = bcrypt.compareSync(password, req.app.locals.adminPwHash);
-    if (!match) {
-      res.json(response);
-      return;
+        // Check if voter is invalidated
+        if (!voters[idx].isValid) {
+            res.json(response);
+            return;
+        }
+        id = voters[idx].id;
     } else {
-      req.app.locals.adminPw = password;
+        // Admin
+        // Check if password is correct
+        let match = bcrypt.compareSync(password, req.app.locals.adminPwHash);
+        if (!match) {
+            res.json(response);
+            return;
+        } else {
+            req.app.locals.adminPw = password;
+        }
     }
-  }
 
-  //Successful, set session states
-  req.session.username = username;
-  req.session.userid = id;
-  if (username === ADMIN) {
-    req.session.userType = ADMIN;
-	response.userType = ADMIN;
-	
-	// Prepare workbook
-    XlsxPopulate.fromBlankAsync().then(workbook => {
-      return workbook.toFileAsync("./Votes.xlsx", {
-        password: req.app.locals.adminPw
-      });
-	});
-	
-	// Try to load files if possible
-	let data = local.load()
-	for (let key in data) {
-		req.app.locals[key] = data[key];
-	}
+    //Successful, set session states
+    req.session.username = username;
+    req.session.userid = id;
+    if (username === ADMIN) {
+        req.session.userType = ADMIN;
+        response.userType = ADMIN;
 
-  } else {
-    req.session.userType = USER;
-    response.userType = USER;
-  }
-  response.success = true;
-  response.id = id;
-  res.json(response);
+        // Prepare workbook
+        XlsxPopulate.fromBlankAsync().then(workbook => {
+            return workbook.toFileAsync("./Votes.xlsx", {
+                password: req.app.locals.adminPw
+            });
+        });
+
+        // Try to load files if possible
+        let data = local.load(req.app.locals.adminPw);
+        for (let key in data) {
+            req.app.locals[key] = data[key];
+        }
+    } else {
+        req.session.userType = USER;
+        response.userType = USER;
+    }
+    response.success = true;
+    response.id = id;
+    res.json(response);
 });
 
 module.exports = router;
