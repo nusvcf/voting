08761d422cadc33237f468b2608e8d7c9c9cde8d
Yen Ter <leeyenter@gmail.com>
2023-04-27 09:56:52 +0800

    Display last seen

--- frontend/src/components/admin/VotersPage.tsx
+++ frontend/src/components/admin/VotersPage.tsx
@@ -1,175 +1,165 @@
-import { format } from "date-fns";
-import React, { Component } from "react";
+import {format} from "date-fns";
+import React, {useEffect, useState} from "react";
 import LoadingDiv from "./LoadingDiv";
 
 interface Voter {
-    id: string;
-    username: string;
-    password: string;
-    last_seen?: string;
-    invalidated?: string;
+  id: string;
+  username: string;
+  password: string;
+  last_seen?: string;
+  invalidated?: string;
 }
 
-class VotersPage extends Component<any, any> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            fetchingData: true,
-            addStart: 1,
-            addEnd: 1,
-            voters: []
-        };
+const INVALIDATION_MESSAGE = "Are you sure you want to invalidate this user?\n\nThey will no longer be able to cast any more votes.";
+const DELETE_MESSAGE = "Are you sure you want to DELETE this user?\n\nThis will remove the user, and remove all past votes cast by them.";
 
-        this.fetchData();
+function VoterRow(props: { voter: Voter, fetchData: () => void }) {
+  const invalidClass = props.voter.invalidated ? "invalid" : ""
+
+  const invalidateVoter = () => {
+    if (window.confirm(INVALIDATION_MESSAGE)) {
+      fetch(`/admin/voters/${props.voter.id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"}
+      }).then(props.fetchData);
     }
+  };
 
-    fetchData = () => {
-        fetch("/admin/voters")
-            .then(data => data.json())
-            .then((json: Voter[]) => {
-                this.setState({ voters: json, fetchingData: false });
-                const nextRange = parseInt(json[json.length-1].username) + 1;
-                this.setState({
-                    addStart: nextRange,
-                    addEnd: nextRange,
-                })
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    };
-
-    updateAddStart = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ addStart: parseInt(e.target.value) });
-    };
-    updateAddEnd = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ addEnd: parseInt(e.target.value) });
-    };
-
-    addVoters = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        // Do a fetch
-        fetch("/admin/voters", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                start: this.state.addStart,
-                end: this.state.addEnd
-            })
-        }).then(() => {
-            this.fetchData();
-        });
-        this.setState({
-            addStart: this.state.addEnd + 1,
-            addEnd: this.state.addEnd + 1
-        });
-    };
-
-    invalidateVoter = (id: string) => {
-        if (
-            window.confirm(
-                "Are you sure you want to invalidate this user?\n\nThey will no longer be able to cast any more votes."
-            )
-        ) {
-            fetch("/admin/voters/" + id, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(() => {
-                this.fetchData();
-            });
-        }
-    };
-
-    deleteVoter = (id: string) => {
-        if (
-            window.confirm(
-                "Are you sure you want to DELETE this user?\n\nThis will remove the user, and remove all past votes cast by them."
-            )
-        ) {
-            fetch("/admin/voters/" + id, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(() => {
-                this.fetchData();
-            });
-        }
-    };
-
-    render() {
-        if (this.state.fetchingData) {
-            return <LoadingDiv show={true} />
-        }
-
-        let rows = this.state.voters.map((voter: Voter, i: number) => {
-            const invalidClass = voter.invalidated ? "invalid" : ""
-
-            return (
-              <tr key={i}>
-                  <td className={invalidClass}>
-                      {voter.username}
-                  </td>
-                  <td className={invalidClass}>
-                      <span className="code">{voter.password}</span>
-                  </td>
-                  <td>
-                      <div className="tbl-btns">
-                      {!voter.invalidated && (
-                        <button
-                          className="btn-secondary"
-                          onClick={() => this.invalidateVoter(voter.id)}
-                        >
-                            Invalidate
-                        </button>
-                      )}
-                      {voter.invalidated && <span className='invalidated-on'>Invalidated on {format(new Date(voter.invalidated), 'd MMM y, HH:mm')}</span>}
-                      <button
-                        className="btn-warn"
-                        onClick={() => this.deleteVoter(voter.id)}
-                      >
-                          Delete
-                      </button>
-                      </div>
-                  </td>
-              </tr>
-            );
-        });
-
-        return (
-            <div id="voters-page">
-                <div className="input-group input-group-row">
-                    <input
-                        type="number"
-                        value={this.state.addStart}
-                        onChange={this.updateAddStart}
-                    />
-                    <input
-                        type="number"
-                        value={this.state.addEnd}
-                        onChange={this.updateAddEnd}
-                    />
-                    <button onClick={this.addVoters}>
-                        +&nbsp;&nbsp;Add Voters
-                    </button>
-                </div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Username</th>
-                            <th>Password</th>
-                            <th>&nbsp;</th>
-                        </tr>
-                    </thead>
-                    <tbody>{rows}</tbody>
-                </table>
-            </div>
-        );
+  const deleteVoter = () => {
+    if (window.confirm(DELETE_MESSAGE)) {
+      fetch(`/admin/voters/${props.voter.id}`, {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json"}
+      }).then(props.fetchData);
     }
+  };
+
+  let timestampText = '';
+  if (props.voter.invalidated) {
+    timestampText = 'Invalidated on ' + format(new Date(props.voter.invalidated), "d MMM y, HH:mm")
+  } else if (props.voter.last_seen) {
+    timestampText = 'Last seen on ' + format(new Date(props.voter.last_seen), "d MMM y, HH:mm")
+  } else {
+    timestampText = 'Not yet logged in'
+  }
+
+  return <tr className={invalidClass}>
+    <td className='to-strikethrough'>
+      {props.voter.username}
+    </td>
+    <td className='to-strikethrough'>
+      <span className="code">{props.voter.password}</span>
+    </td>
+    <td>
+      <span
+        className="invalidated-on">{timestampText}</span>
+    </td>
+    <td>
+      <div className="tbl-btns">
+        {!props.voter.invalidated && (
+          <button
+            className="btn-secondary"
+            onClick={invalidateVoter}
+          >
+            Invalidate
+          </button>
+        )}
+        <button
+          className="btn-warn"
+          onClick={deleteVoter}
+        >
+          Delete
+        </button>
+      </div>
+    </td>
+  </tr>;
+}
+
+const VotersPage = () => {
+  const [fetchingData, setFetchingData] = useState(true);
+  const [addStart, setAddStart] = useState(1);
+  const [addEnd, setAddEnd] = useState(1);
+  const [voters, setVoters] = useState<Voter[]>([]);
+
+  const fetchData = () => {
+    fetch("/admin/voters")
+      .then(data => data.json())
+      .then((receivedVoters: Voter[]) => {
+        const nextRange = parseInt(receivedVoters[receivedVoters.length - 1].username) + 1;
+
+        setFetchingData(false)
+        setVoters(receivedVoters)
+        setAddStart(nextRange);
+        setAddEnd(nextRange);
+      })
+      .catch(error => {
+        console.error(error);
+        // if 401, reload page
+      });
+  };
+
+
+  const updateAddStart = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddStart(parseInt(e.target.value))
+  };
+
+  const updateAddEnd = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddEnd(parseInt(e.target.value))
+  };
+
+  const addVoters = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    // Do a fetch
+    fetch("/admin/voters", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        start: addStart,
+        end: addEnd
+      })
+    }).then(fetchData);
+
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  if (fetchingData) return <LoadingDiv show={true}/>;
+
+  return (
+    <div id="voters-page">
+      <div className="input-group input-group-row">
+        <input
+          type="number"
+          value={addStart}
+          onChange={updateAddStart}
+        />
+        <input
+          type="number"
+          value={addEnd}
+          onChange={updateAddEnd}
+        />
+        <button onClick={addVoters}>
+          +&nbsp;&nbsp;Add Voters
+        </button>
+      </div>
+      <table>
+        <thead>
+        <tr>
+          <th>Username</th>
+          <th>Password</th>
+          <th>Last Seen</th>
+          <th>&nbsp;</th>
+        </tr>
+        </thead>
+        <tbody>{voters.map((voter: Voter) =>
+          <VoterRow key={voter.id} voter={voter} fetchData={fetchData}/>)}</tbody>
+      </table>
+    </div>
+  )
 }
 
 export default VotersPage;
